af8e4fd0fb60fe296d94ffe85c2d603f762169a7
Akanksha <akanksha012btit21@igdtuw.ac.in>
2024-01-23 10:00:00 +0530

    day2

---
+++ script.js
@@ -0,0 +1,26 @@
+// Function to fetch the Spotify API
+async function fetchSpotifyAPI() {
+    const CLIENT_ID = process.env.CLIENT_ID;
+    const playlistId = "37i9dQZF1DX9tPFwDMOaN1"; // Playlist ID for "Nostalgic Bollywood Songs (2000-2010)"
+    const response = await fetch(`https://api.spotify.com/v1/playlists/${playlistId}/tracks`, {
+        headers: {
+            Authorization: "Bearer d128c3f604ef430ab72613ccd5b51c00" 
+        },
+    })
+    .then(response => response.json())
+    .then(data => {
+      // Randomly select a track
+      const randomIndex = Math.floor(Math.random() * data.items.length);
+      const chosenTrack = data.items[randomIndex].track;
+  
+      // Open Spotify with the chosen track
+      const spotifyUri = `spotify:track:${chosenTrack.id}`;
+      window.location.href = spotifyUri;
+    })
+    .catch(error => console.error('Error fetching playlist tracks:', error));
+}
+
+
+document.getElementById("btn").addEventListener("click", fetchSpotifyAPI);
+
+

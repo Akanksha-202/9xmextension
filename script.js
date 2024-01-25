76e46fa085bbf9f0d0422a2da3c3029da0e9f8c0
Akanksha <akanksha012btit21@igdtuw.ac.in>
2024-01-25 11:47:07 +0530

    Update .env and script.js files

--- script.js
+++ script.js
@@ -1,26 +1,37 @@
-// Function to fetch the Spotify API
 async function fetchSpotifyAPI() {
-    const CLIENT_ID = process.env.CLIENT_ID;
-    const playlistId = "37i9dQZF1DX9tPFwDMOaN1"; // Playlist ID for "Nostalgic Bollywood Songs (2000-2010)"
+    const CLIENT_ID = '';
+    const CLIENT_SECRET = '';
+    const playlistId = "2hEfevLS7w2zcWWGdEmbDj"; // Playlist ID for "Nostalgic Bollywood Songs (2000-2010)"
+
+    // Request access token
+    const tokenResponse = await fetch("https://accounts.spotify.com/api/token", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/x-www-form-urlencoded",
+            "Authorization": `Basic ${btoa(`${CLIENT_ID}:${CLIENT_SECRET}`)}` // Base64 encode the client ID and client secret
+        },
+        body: "grant_type=client_credentials"
+    });
+
+    const tokenData = await tokenResponse.json();
+
+    // Use the obtained access token to make a request to the Spotify API
     const response = await fetch(`https://api.spotify.com/v1/playlists/${playlistId}/tracks`, {
         headers: {
-            Authorization: "Bearer " 
+            Authorization: `Bearer ${tokenData.access_token}`
         },
     })
-    .then(response => response.json())
-    .then(data => {
-      // Randomly select a track
-      const randomIndex = Math.floor(Math.random() * data.items.length);
-      const chosenTrack = data.items[randomIndex].track;
-  
-      // Open Spotify with the chosen track
-      const spotifyUri = `spotify:track:${chosenTrack.id}`;
-      window.location.href = spotifyUri;
-    })
-    .catch(error => console.error('Error fetching playlist tracks:', error));
+        .then(response => response.json())
+        .then(data => {
+            console.log(data);
+            // Randomly select a track
+            const randomIndex = Math.floor(Math.random() * data.items.length);
+            const chosenTrack = data.items[randomIndex].track;
+            // Open Spotify app or website
+            window.location.href = `spotify:track:${chosenTrack.id}`;
+
+        })
+        .catch(error => console.error('Error fetching playlist tracks:', error));
 }
 
-
 document.getElementById("btn").addEventListener("click", fetchSpotifyAPI);
-
-
